d009f79dbce2a12c5014a7738e591de77a9fcbfb
Takahito Nakano <kano@ponko2.jp>
2024-12-14 20:22:53 +0900

    build: DevContainerの設定を追加

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+git config --global --add safe.directory /workspaces/refined-amazon
+
+npm uninstall -g pnpm
+sudo corepack enable pnpm
+corepack install --global pnpm
+pnpm config set store-dir ~/.local/share/pnpm/store
+
+pnpm install --force
