2c8f810f2d609f1ffd661ab2c71ed7ad03528775
Takahito Nakano <kano@ponko2.jp>
2024-12-14 22:37:05 +0900

    feat: Amazon直販商品で絞り込み

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
+  manifest: {
+    name: "Refined Amazon",
+    host_permissions: ["*://www.amazon.co.jp/*"],
+    permissions: [
+      "declarativeNetRequest",
+      "declarativeNetRequestWithHostAccess",
+    ],
+  },
 });
